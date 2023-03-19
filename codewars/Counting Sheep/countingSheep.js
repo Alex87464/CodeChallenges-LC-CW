6159b7d717a95cf41ede09a55e9593205a8820e4
Alex87464 <alexoliva87464@gmail.com>
2023-03-19 06:50:13 -0300

    Problem solved added

---
+++ codewars/Counting Sheep/countingSheep.js
@@ -0,0 +1,29 @@
+// Conting Sheep
+// https://www.codewars.com/kata/54edbc7200b811e956000556
+
+
+// Descomentar la linea de abajo para que pueda tomar un array la funcion 
+// const arrayOfSheep = [true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true, false, false, true , true,  true,  true,  true , false, false, true,  true];
+
+function countSheeps(arrayOfSheep) {
+
+    let aux = [];
+    
+    for(let i = 0; i < arrayOfSheep.length; i++){
+
+        if(arrayOfSheep[i] == true ){
+            
+            aux.push(arrayOfSheep[i]);
+
+        }
+    
+    }
+
+    // console.log(aux);
+    return aux.length;
+    
+}
+
+
+// Descomentar esta linea para ver en consola el resultado
+// console.log(countSheeps(arrayOfSheep));
