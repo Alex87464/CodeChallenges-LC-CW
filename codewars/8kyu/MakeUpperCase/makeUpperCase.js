9c8f9408b74978c25458454c662d668d5b04a86c
Alex87464 <alexoliva87464@gmail.com>
2023-03-23 06:16:33 -0300

    Solved problem added

---
+++ codewars/8kyu/MakeUpperCase/makeUpperCase.js
@@ -0,0 +1,14 @@
+// MakeUpperCase
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+const str = 'hello world';
+
+function makeUpperCase(str){
+
+    console.log(str.toUpperCase());
+    return str.toUpperCase();
+
+}
+
+
+makeUpperCase(str); // Output -> 'HELLO WORLD'
