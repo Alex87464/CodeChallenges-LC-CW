d3ac3c9c1594da477a819dbe78f4d6015252ff7f
Alex87464 <alexoliva87464@gmail.com>
2023-03-28 03:45:40 -0300

    Solved problem added

---
+++ codewars/8kyu/Stringy Strings/stringy.js
@@ -0,0 +1,29 @@
+// Stringy Strings
+// https://www.codewars.com/kata/563b74ddd19a3ad462000054
+
+const size = '10'
+
+function stringy(size) {
+
+    const arr = [];
+
+    let counter1 = 0;
+    let counter2 = 2;
+
+    while(counter1 < size){
+        counter1++;
+
+        counter2 = (counter2+1)%2;
+        console.log(`Contador 1: ${counter1}, Contador 2: ${counter2}`);
+        arr.push(counter2);
+    }
+
+    console.log(arr.join(''))
+    return(arr.join(''));
+
+
+
+}
+
+
+stringy(size);
