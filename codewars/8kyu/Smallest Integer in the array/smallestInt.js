e160b36706106f12e1fe1c375cbbf2b44dcf01c2
Alex87464 <alexoliva87464@gmail.com>
2023-03-29 20:54:48 -0300

    Problem added

---
+++ codewars/8kyu/Smallest Integer in the array/smallestInt.js
@@ -0,0 +1,9 @@
+// Find the smallest integer in the array
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
+const numbers = [34, 15, 88, 2];
+function smallestInt(numbers) {
+    
+}
+
+smallestInt(numbers) // Output -> 2
