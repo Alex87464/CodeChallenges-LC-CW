2c9030163dbeb9e20a1fed4617a8a075600818ae
Alex87464 <alexoliva87464@gmail.com>
2023-03-29 20:51:18 -0300

    Solved problem added

--- codewars/8kyu/Square n Sum/squareSum.js
+++ codewars/8kyu/Square n Sum/squareSum.js
@@ -6,6 +6,10 @@ const numbers = [1, 2, 2];
 
 function squareSum(numbers){
 
+    const squared = numbers.map(x => Math.pow(x, 2));
+    const sum = squared.reduce((acumulator, element) => acumulator + element, 0);
+    return sum;
+
 }
 
-squareSum(numbers);
+squareSum(numbers); // Output -> 9 [1^2, 2^2, 2^2] 1+2+2 = 9
