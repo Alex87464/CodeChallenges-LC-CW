42dc6da9b82eeb34df66d8d4cc84f6c09f01d164
Alex87464 <alexoliva87464@gmail.com>
2023-03-30 19:45:39 -0300

    Solved problem added

--- codewars/7kyu/Disemvowel Trolls/disemvowel.js
+++ codewars/7kyu/Disemvowel Trolls/disemvowel.js
@@ -4,4 +4,24 @@
 
 function disemvowel(str) {
 
+    const vowels = ['a','e','i','o','u'];
+    let result = '';
+
+    for(let i = 0; i < str.length; i++){
+        const char = str[i].toLowerCase();
+        
+        if(vowels.includes(char)){
+            // Si la letra actual es una vocal no hacer nada
+        } else{
+            // Si es una vocal agregarla a result
+            result += str[i]
+        }
+    }
+
+    console.log(result);
+    return result;
+
+
 }
+
+disemvowel("Hola como estas"); // Output -> Hl cm sts
