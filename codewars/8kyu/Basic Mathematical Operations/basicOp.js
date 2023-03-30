61f3084d05b9b4e0532ce643842302972c025734
Alex87464 <alexoliva87464@gmail.com>
2023-03-29 21:08:48 -0300

    Solved problem added

--- codewars/8kyu/Basic Mathematical Operations/basicOp.js
+++ codewars/8kyu/Basic Mathematical Operations/basicOp.js
@@ -2,7 +2,16 @@
 // https://www.codewars.com/kata/57356c55867b9b7a60000bd7
 
 
-function basicOp(operation, value1, value2)
-{
-  // Code
+function basicOp(operation, value1, value2){
+
+    if(operation === '+'){
+        return value1 + value2;
+    } else if(operation === '-'){
+        return value1 - value2;
+    } else if(operation === '*'){
+        return value1 * value2;
+    } else if(operation === '/'){
+        return value1 / value2;
+    }
+
 }
