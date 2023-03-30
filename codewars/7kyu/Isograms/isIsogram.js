bb8f9a07ae15993156ea62a7b59d0ebc14e07cce
Alex87464 <alexoliva87464@gmail.com>
2023-03-30 20:16:53 -0300

    Solved problem added

--- codewars/7kyu/Isograms/isIsogram.js
+++ codewars/7kyu/Isograms/isIsogram.js
@@ -3,4 +3,23 @@
 
 function isIsogram(str){
 
+    str = str.toLowerCase();
+
+    // Creamos un objeto para almacenar las letras de la palabra
+    let letters = {};
+    // Recorremos cada letra de la palabra
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i];
+        // Si la letra ya ha sido vista antes, entonces no es un isograma
+        if (letters[letter]) {
+            return false;
+        }
+        // Si la letra no ha sido vista antes, la almacenamos en el objeto
+        letters[letter] = true;
+    }
+    // Si llegamos hasta aquí, entonces la palabra es un isograma
+    return true;
+
 }
+
+isIsogram('Dermatoglyphics'); // Output -> True
