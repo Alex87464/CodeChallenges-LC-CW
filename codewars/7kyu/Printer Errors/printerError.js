4ad7511eb0510087685ba1456b12626a23031b19
Alex87464 <alexoliva87464@gmail.com>
2023-03-31 05:09:06 -0300

    Solved problem added

--- codewars/7kyu/Printer Errors/printerError.js
+++ codewars/7kyu/Printer Errors/printerError.js
@@ -3,10 +3,28 @@
 
 function printerError(s) {
 
+    // Posible solucion:
+    // 1. Crear una variable auxiliar como contador
+    // 2. Recorrer el string
+    // 3. Crear una variable 'letra' que tome el valor del char en la posicion 'i' de mi texto
+    // 4. Creo la condicion en el caso de que la letra este fuera del rango de 'a' o 'm' aumente mi contador
+    // 5. Retorar: `${contador}/${s.length}`
 
+
+    let contador = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let letra = s[i];
+        if (letra < "a" || letra > "m") {
+            contador++;
+        }
+    }
+    
+    console.log(`${contador}/${s.length}`) // `contador/s.length`
+    return `${contador}/${s.length}`;
 
 }
 
-printerError('aaaxbbbbyyhwawiwjjjwwm') // Output -> "8/22"
+printerError('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz') // Output -> "8/22"
 
 // Note: Letters 'a' to 'm' are allowed
