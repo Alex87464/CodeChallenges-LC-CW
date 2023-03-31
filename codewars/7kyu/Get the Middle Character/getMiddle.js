10bfd64b1502de765001c57abbd50b340ff4a4e5
Alex87464 <alexoliva87464@gmail.com>
2023-03-31 05:33:17 -0300

    Solved problem added

--- codewars/7kyu/Get the Middle Character/getMiddle.js
+++ codewars/7kyu/Get the Middle Character/getMiddle.js
@@ -4,4 +4,22 @@
 
 function getMiddle(s) {
 
+    // Paso 1 chequeo si la palabra es par o impar
+    // Esto me sirve para saber si voy a tener que retornar dos letras (caso par) o solo una (caso impar)
+    // Ejemplo: 'oso' -> 3 letras -> impar -> 's' || 'cosa' -> 4 letras -> par -> 'os'
+
+    if(s.length%2 == 0){
+        console.log(s[s.length/2-1] + s[s.length/2])
+        return s[s.length/2-1] + s[s.length/2];
+    } else{
+        console.log(s[Math.floor(s.length/2)]);
+        return s[Math.floor(s.length/2)];
+    }
+
+    // Operador ternario
+    // return s.length%2 == 0 ? s[s.length/2-1] + s[s.length/2] : s[Math.floor(s.length/2)];
 }
+// Arrow solution
+// const getMiddle = (s) => s.length%2 == 0 ? s[s.length/2-1] + s[s.length/2] : s[Math.floor(s.length/2)];
+
+getMiddle('a') // Output ->  
