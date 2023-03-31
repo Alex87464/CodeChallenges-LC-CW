dee449a3480d3879f7d507d6abab62d5f6291a35
Alex87464 <alexoliva87464@gmail.com>
2023-03-31 04:47:10 -0300

    Problem added

---
+++ codewars/7kyu/Printer Errors/printerError.js
@@ -0,0 +1,12 @@
+// Printer Errors
+// https://www.codewars.com/kata/56541980fa08ab47a0000040
+
+function printerError(s) {
+
+
+
+}
+
+printerError('aaaxbbbbyyhwawiwjjjwwm') // Output -> "8/22"
+
+// Note: Letters 'a' to 'm' are allowed
