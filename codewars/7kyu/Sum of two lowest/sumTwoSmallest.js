1797c1907b5e55082d89a18c18b2d39345141095
Alex87464 <alexoliva87464@gmail.com>
2023-03-31 04:40:43 -0300

    Solved problem added

--- codewars/7kyu/Sum of two lowest/sumTwoSmallest.js
+++ codewars/7kyu/Sum of two lowest/sumTwoSmallest.js
@@ -1,7 +1,14 @@
 // Sum of two lowest positive integers
 // https://www.codewars.com/kata/558fc85d8fd1938afb000014
 
+const numbers = [19, 5, 42, 2, 77]
 
 function sumTwoSmallestNumbers(numbers) {  
-    
+
+    numbers = numbers.sort(function(a, b) { return a - b; });
+    console.log(numbers[0]+numbers[1]);
+    return numbers[0]+numbers[1];
+
 }
+
+sumTwoSmallestNumbers(numbers); // Output -> 7 -> 5+2
