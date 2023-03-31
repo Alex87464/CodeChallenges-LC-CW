0896471dd71171a1b10a74d591b9c07b2fe13b4f
Alex87464 <alexoliva87464@gmail.com>
2023-03-30 22:32:56 -0300

    Solved problem added

--- codewars/7kyu/Highest and Lowest/highAndLow.js
+++ codewars/7kyu/Highest and Lowest/highAndLow.js
@@ -1,9 +1,17 @@
 // Highest and Lowest
 // https://www.codewars.com/kata/554b4ac871d6813a03000035
 
+const numbers = '1 2 3';
 
 function highAndLow(numbers){
 
-    
+    let num = numbers.split(" ").map(Number);
+    var max = Math.max.apply(null, num);
+    var min = Math.min.apply(null, num);
+
+    console.log(`${max.toString()} ${min.toString()}`);
+    return `${max.toString()} ${min.toString()}`;
     
 }
+
+highAndLow(numbers); // Output -> 3 1
