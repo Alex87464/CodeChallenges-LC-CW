9f1901e84791bb6acd59bc466bcfb3d680de82d6
Alex87464 <alexoliva87464@gmail.com>
2023-04-01 06:18:55 -0300

    Solved problem added

--- codewars/8kyu/Calculate BMI/bmi.js
+++ codewars/8kyu/Calculate BMI/bmi.js
@@ -4,8 +4,23 @@
 
 function bmi(weight, height) {
 
+    let bmi = weight / Math.pow(height, 2);
+    console.log(bmi);
+
+    if(bmi <= 18.5) {
+        console.log('Underweight');
+    } else if(bmi <= 25){
+        console.log('Normal');
+    } else if(bmi <= 30){
+        console.log('Overweight');
+    } else if(bmi > 30){
+        console.log('Obese')
+    }
     
+    // Solucion con operador ternario
+    // return bmi <= 18.5 ? 'Underweight' : bmi <= 25 ? 'Normal': bmi <= 30 ? 'Overweight': 'Obese';
+
 
 }
 
-bmi(80, 1.80) // Output -> 'Normal'
+bmi(80, 1.80); // Output -> 'Normal'
