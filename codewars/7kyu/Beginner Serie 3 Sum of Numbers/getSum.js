68f06778752350ccf9f8e9b4c192935fd9f8aa32
Alex87464 <alexoliva87464@gmail.com>
2023-04-01 19:38:08 -0300

    Solved problem added

---
+++ codewars/7kyu/Beginner Serie 3 Sum of Numbers/getSum.js
@@ -0,0 +1,23 @@
+// Beginner Series #3 Sum of Numbers
+// https://www.codewars.com/kata/55f2b110f61eb01779000053
+
+
+function getSum(a, b){
+
+    let start = a;
+    let end = b;
+
+    if (a > b) {
+      start = b;
+      end = a;
+    }
+
+    let arr = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    
+    const initialValue = 0;
+    arr = arr.reduce((acumulator, currentValue) => acumulator + currentValue, initialValue)
+
+    return arr;
+  }
+
+getSum(5, -1);
