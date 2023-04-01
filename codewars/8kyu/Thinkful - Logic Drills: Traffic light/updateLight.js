c30b4bc353f7a35b526b6fc0ce934c3f4e40946b
Alex87464 <alexoliva87464@gmail.com>
2023-04-01 05:27:26 -0300

    Problem added

---
+++ codewars/8kyu/Thinkful - Logic Drills: Traffic light/updateLight.js
@@ -0,0 +1,10 @@
+// Thinkful - Logic Drills: Traffic light
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+function updateLight(current) {
+
+
+
+}
+
+updateLight('green'); // Output -> 'yellow'
