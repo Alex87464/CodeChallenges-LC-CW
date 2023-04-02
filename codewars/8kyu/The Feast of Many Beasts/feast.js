c523395bc414c4d88c3d5401e5f3c879220c544a
Alex87464 <alexoliva87464@gmail.com>
2023-04-02 11:11:25 -0300

    Problem added

---
+++ codewars/8kyu/The Feast of Many Beasts/feast.js
@@ -0,0 +1,10 @@
+// The Feast of Many Beasts
+// https://www.codewars.com/kata/5aa736a455f906981800360d
+
+function feast(beast, dish) {
+
+
+
+}
+
+feast("chickadee", "chocolate cake"); // Output -> true
