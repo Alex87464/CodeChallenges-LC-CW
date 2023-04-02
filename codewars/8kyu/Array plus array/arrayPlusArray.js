299da33f33700a7b77d141a00739ca05a1ca6a65
Alex87464 <alexoliva87464@gmail.com>
2023-04-02 10:52:42 -0300

    Problem added

---
+++ codewars/8kyu/Array plus array/arrayPlusArray.js
@@ -0,0 +1,13 @@
+// Array plus array
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+function arrayPlusArray(arr1, arr2) {
+
+
+
+}
+
+arrayPlusArray(arr1, arr2);
