c4f024cae69bd0adabd4f10686714bdabdbd5cbd
Alex87464 <alexoliva87464@gmail.com>
2023-04-02 11:31:17 -0300

    Solved problem added

--- codewars/8kyu/Twice as old/twiceAsOld.js
+++ codewars/8kyu/Twice as old/twiceAsOld.js
@@ -3,6 +3,14 @@
 
 function twiceAsOld(dadYearsOld, sonYearsOld) {
 
-
-
+    let yearsAgoTwiceAsOld = Math.abs(dadYearsOld - 2 * sonYearsOld);
+    if (dadYearsOld < 2 * sonYearsOld) {
+      return yearsAgoTwiceAsOld;
+    } else if (dadYearsOld > 2 * sonYearsOld) {
+      return yearsAgoTwiceAsOld;
+    } else {
+      return 0;
+    }
 }
+
+twiceAsOld(36, 7); // Output -> 22
