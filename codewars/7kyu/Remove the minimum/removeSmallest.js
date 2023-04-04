4f9af6db5c87d6c10fbf1feb6d2024efb84b1132
Alex87464 <alexoliva87464@gmail.com>
2023-04-04 18:33:19 -0300

    Problem added

---
+++ codewars/7kyu/Remove the minimum/removeSmallest.js
@@ -0,0 +1,10 @@
+// Remove the minimum
+// https://www.codewars.com/kata/563cf89eb4747c5fb100001b
+
+function removeSmallest(numbers) {
+
+
+
+}
+
+removeSmallest([10, 3, 5, 1, 6, 7]) // Output -> 1
