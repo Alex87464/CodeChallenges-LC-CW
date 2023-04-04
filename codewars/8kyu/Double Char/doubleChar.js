32ecc19c0f1af139ca1e7a13c41b8c8e341d6fd7
Alex87464 <alexoliva87464@gmail.com>
2023-04-04 19:01:23 -0300

    Problem added

---
+++ codewars/8kyu/Double Char/doubleChar.js
@@ -0,0 +1,11 @@
+// Double Char
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+function doubleChar(str){
+
+    
+
+}
+
+
+doubleChar('Hola mundo') // Output -> 'HHoollaa  mmuunnddoo'
