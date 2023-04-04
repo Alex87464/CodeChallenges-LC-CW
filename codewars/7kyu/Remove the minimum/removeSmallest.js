c279268b70b851335cb4f2d9781dae47e6305b3a
Alex87464 <alexoliva87464@gmail.com>
2023-04-04 18:55:46 -0300

    Solved problem added

--- codewars/7kyu/Remove the minimum/removeSmallest.js
+++ codewars/7kyu/Remove the minimum/removeSmallest.js
@@ -2,9 +2,18 @@
 // https://www.codewars.com/kata/563cf89eb4747c5fb100001b
 
 function removeSmallest(numbers) {
+    // si el array esta vacio lo retorno
+    if(arr.length === 0) {
+        return arr;
+    }
 
+    // minIndex toma el indice o posicion del numero mas chico
+    const minIndex = arr.indexOf(Math.min(...ar));
 
+    // elimino el valor mas chico usando el metodo slice y concateno el siguiente numero al valor minimo
+    const result = arr.slice(0, minIndex).concat(arr.slice(minIndex) + 1);
 
+    return result;
 }
 
-removeSmallest([10, 3, 5, 1, 6, 7]) // Output -> 1
+removeSmallest([2, 2, 1, 2, 1]); // Output -> [2, 2, 2, 1]
