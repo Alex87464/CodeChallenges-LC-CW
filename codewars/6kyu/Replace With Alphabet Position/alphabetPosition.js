dd3fc40de6a537ce3b5e96400eb32b7637f1a691
Alex87464 <alexoliva87464@gmail.com>
2023-04-05 10:20:14 -0300

    Problem added

---
+++ codewars/6kyu/Replace With Alphabet Position/alphabetPosition.js
@@ -0,0 +1,13 @@
+// Replace With Alphabet Position
+// https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+function alphabetPosition(text) {
+    
+    return text;
+
+}
+
+
+
+alphabetPosition("The sunset sets at twelve o' clock."); 
+// Expected output -> "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
