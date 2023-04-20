0d892702a09460ff255f602beee23d398d12c84f
Alex87464 <alexoliva87464@gmail.com>
2023-04-20 11:08:52 -0300

    Problem added

---
+++ codewars/7kyu/Mumbling/accum.js
@@ -0,0 +1,16 @@
+// Mumbling
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+function accum(s) {
+
+    
+
+}
+
+accum("abcd"); // Output -> "A-Bb-Ccc-Dddd"
+
+/*Examples
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt" 
+*/
