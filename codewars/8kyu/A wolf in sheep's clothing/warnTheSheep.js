3eb5caa5fbbc78a51ac8603b62e626cfa65b2399
Alex87464 <alexoliva87464@gmail.com>
2023-05-13 07:50:55 -0300

    Problem solved

--- codewars/8kyu/A wolf in sheep's clothing/warnTheSheep.js
+++ codewars/8kyu/A wolf in sheep's clothing/warnTheSheep.js
@@ -1,15 +1,20 @@
 // A wolf in sheep's clothing
 // https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
 
-const queue = ["sheep", "sheep", "sheep", "wolf", "sheep"];
+let queue = ['sheep', 'sheep', 'wolf', 'sheep', 'sheep', 'sheep', 'sheep']; // [7, 6, 5 -> wolf, 4, 3, 2, 1] 
 
 function warnTheSheep(queue) {
-
-    console.log(queue);
-
+    let counter = queue.length;
+    
+    for (let i = 0; i < queue.length; i++) {
+        counter--;    
+        if(queue[queue.length-1] === 'wolf') return 'Pls go away and stop eating my sheep';
+        if(queue[i] === 'wolf') {
+            return `Oi! Sheep number ${counter}! You are about to be eaten by a wolf!`; 
+        }
+    }
 }
 
-
-warnTheSheep(queue)
+warnTheSheep(queue); // Output -> 'Oi! Sheep number 1! You are about to be eaten by a wolf!'
 
 
