719c3297dd8919c5e916b6d7872c819447ececbf
Alex87464 <alexoliva87464@gmail.com>
2023-05-13 05:13:32 -0300

    Problem added

---
+++ codewars/8kyu/A wolf in sheep's clothing/warnTheSheep.js
@@ -0,0 +1,15 @@
+// A wolf in sheep's clothing
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+
+const queue = ["sheep", "sheep", "sheep", "wolf", "sheep"];
+
+function warnTheSheep(queue) {
+
+    console.log(queue);
+
+}
+
+
+warnTheSheep(queue)
+
+
