106cab7c32d4af221dbcef71a4c728f9d2411bed
Alex87464 <alexoliva87464@gmail.com>
2023-07-15 09:28:49 -0300

    Added new problem

---
+++ codewars/7kyu/Suzuki needs help lining up his students/lineupStudents.js
@@ -0,0 +1,7 @@
+// Suzuki needs help lining up his students!
+// https://www.codewars.com/kata/5701800886306a876a001031
+
+let students = "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi";
+function lineupStudents(students) {
+
+}
