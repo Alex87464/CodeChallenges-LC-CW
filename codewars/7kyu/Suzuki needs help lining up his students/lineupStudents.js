ad84f4fc4581c5c0b125135e160ccab6be6780fa
Alex87464 <alexoliva87464@gmail.com>
2023-07-15 11:31:11 -0300

    Solved problem

--- codewars/7kyu/Suzuki needs help lining up his students/lineupStudents.js
+++ codewars/7kyu/Suzuki needs help lining up his students/lineupStudents.js
@@ -2,6 +2,33 @@
 // https://www.codewars.com/kata/5701800886306a876a001031
 
 let students = "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi";
-function lineupStudents(students) {
 
+function lineupStudents(students) {
+  var studentsArray = students.split(' ');
+
+  studentsArray.sort(function(a, b) {
+    if (a.length > b.length) {
+      return -1;
+    } else if (a.length < b.length) {
+      return 1;
+    } else {
+      return b.localeCompare(a); // Orden alfabético descendente si tienen la misma longitud
+    }
+  });
+
+  return studentsArray;
 }
+lineupStudents(students);
+
+// expected output:
+// lst = [
+//   "Takehiko",
+//   "Takayuki",
+//   "Takahiro",
+//   "Takeshi",
+//   "Takeshi",
+//   "Takashi",
+//   "Tadashi",
+//   "Takeo",
+//   "Takao",
+// ];
