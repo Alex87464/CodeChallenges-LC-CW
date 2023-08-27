1fa8a4672fea9f2761d518a2894f5c4e1f49e0ee
Alex87464 <alexoliva87464@gmail.com>
2023-08-27 13:23:51 -0300

    Solved problem

--- codewars/7kyu/Simple Fun 233 Parameter Of Number/parameter.js
+++ codewars/7kyu/Simple Fun 233 Parameter Of Number/parameter.js
@@ -18,10 +18,41 @@ The parameter of the given number.
 
 Example
 For n = 22, the output should be 4.
+
+Both the sum and the product of digits equal 4, and LCM(4, 4) = 4.
+
+For n = 1234, the output should be 120.
+
+1+2+3+4=10 and 1*2*3*4=24, LCM(10,24)=120
+
 */
 
 function parameter(n) {
+    // Transform the number 'n' to an array
+    let numbers = String(n).split('').map( (num) => {
+        return Number(num);
+    });
 
+    let sumResult = numbers.reduce((accumulator, currentValue) =>  accumulator + currentValue);
+    let productResult = numbers.reduce((accumulator, currentValue) =>  accumulator * currentValue);
 
     
+    // To calculate the LCM I can use the GCD (greatest common divisor) formula to determine the LCM
+    function gcd(a, b) { // a & b are the sumResult and productResult
+        for (let temp = b; b !== 0;) {
+            b = a % b;
+            a = temp;
+            temp = b;
+        }
+        return a;
+    }
+
+    let gcdResult = gcd(sumResult, productResult);
+
+    // console.log((sumResult*productResult) / gcdResult)
+    return (sumResult*productResult) / gcdResult;
+
 }
+
+
+parameter(1234);
