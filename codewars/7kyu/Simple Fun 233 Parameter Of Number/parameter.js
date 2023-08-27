ba6224089a72137326f38ce956853894fbbe00a1
Alex87464 <alexoliva87464@gmail.com>
2023-08-27 01:24:17 -0300

    fixed comments in 7 kyu - Mumbling problem & added new problem

---
+++ codewars/7kyu/Simple Fun 233 Parameter Of Number/parameter.js
@@ -0,0 +1,27 @@
+// Simple Fun #223: Parameter Of Number
+// https://www.codewars.com/kata/5902f1839b8e720287000028
+
+/*
+Task
+TODO Let's define a parameter of number n as the least common multiple (LCM) of the sum of its digits and their product.
+
+Calculate the parameter of the given number n.
+
+Input/Output
+[input] integer n
+
+A positive integer. It is guaranteed that no zero appears in n.
+
+[output] an integer
+
+The parameter of the given number.
+
+Example
+For n = 22, the output should be 4.
+*/
+
+function parameter(n) {
+
+
+    
+}
