ea505fb2c339fa256d38d91a58241251ce4f0a43
Alex87464 <alexoliva87464@gmail.com>
2023-09-10 05:59:04 -0300

    added new problem

---
+++ codewars/7kyu/Remove anchor from URL/removeUrlAnchor.js
@@ -0,0 +1,18 @@
+// Remove anchor from URL
+// https://www.codewars.com/kata/51f2b4448cadf20ed0000386
+
+/*
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+
+function removeUrlAnchor(url){
+    // TODO: complete
+    
+}
+
+removeUrlAnchor('www.codewars.com#about') // Expected -> 'www.codewars.com'
