988b5ed01db444bedc204aa62532a85463953040
Alex87464 <alexoliva87464@gmail.com>
2023-09-12 01:06:19 -0300

    Solved problem

--- codewars/7kyu/Testing 1-2-3/number.js
+++ codewars/7kyu/Testing 1-2-3/number.js
@@ -16,5 +16,23 @@ Examples: (Input --> Output)
 */
 
 function number(array) {
+  // Creamos un array vacío para almacenar las líneas numeradas.
+  const numberedLines = [];
 
+  // Usamos un bucle 'for' para iterar a través de las líneas en el array de entrada.
+  for (let i = 0; i < array.length; i++) {
+    // Calculamos el número de línea actual sumando 1 a 'i' (los índices de arrays comienzan en 0).
+    const lineNumber = i + 1;
+
+    // Creamos una cadena que combina el número de línea, un colon y un espacio, y la línea actual.
+    const line = `${lineNumber}: ${array[i]}`;
+
+    // Agregamos la línea numerada al array 'numberedLines'.
+    numberedLines.push(line);
+  }
+
+  // Devolvemos el array 'numberedLines' que contiene todas las líneas numeradas.
+  return numberedLines;
 }
+
+number(["a", "b", "c"]); // ["1: a", "2: b", "3: c"] 
