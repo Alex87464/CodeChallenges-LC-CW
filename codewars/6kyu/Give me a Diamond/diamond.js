d2eee91c18ca44fd71ed124c44f0bd2c7998b33b
Alex87464 <alexoliva87464@gmail.com>
2023-09-12 01:25:46 -0300

    Solved problem

--- codewars/6kyu/Give me a Diamond/diamond.js
+++ codewars/6kyu/Give me a Diamond/diamond.js
@@ -30,7 +30,71 @@ A size 5 diamond:
 "  *\n ***\n*****\n ***\n  *\n"
 */
 
-function diamond(string) {
+
+// Planteo:
+/*
+Tengo que verificar si la entrada de texto retorna un diamante
+Que forma tiene un diamante?
+                                 *
+                                ***
+                                 *
+
+esto en texto se vería de la siguiente forma:
+"*\n***\n *\n"
+console.log(" *\n***\n *\n")
+
+Como sería un diamante de 5?
+  *
+ ***
+*****
+ ***
+  *
+
+ esto en texto se vería de la siguiente forma:
+ "  *\n ***\n*****\n ***\n  *\n"
+    console.log("  *\n ***\n*****\n ***\n  *\n")
+
+Como sería un diamante de 7?
+     *
+    ***
+   *****
+  *******
+   *****
+    ***
+     *
+
+
+
+! Limitaciones
+Si el numero es par o negativo, retornar null
+
+Cual es la estructura logica para hacer un diamante?
+1. Un diamante siempre esta compuesto por un numero de asteristos '*' impar
+2. Un diamante siempre tiene un espacio en blanco ' ' antes y despues de los asteriscos \n
+3. Un diamante siempre tiene un salto de linea \n al final de cada linea
+4. Un diamante siempre tiene un salto de linea \n al final de la ultima linea
+5. Para formar un diamante siempre comienza en 1 y termina en 1 
+*/
+
+function diamond(n) {
+
+    if(n % 2 === 0 || n < 0) {
+        return null;
+    }
+
+    let diamante = '';
+
+    for(let i = 1; i <= n; i += 2) {
+        diamante += ' '.repeat((n - i) / 2) + '*'.repeat(i) + '\n';
+    }
+
+    for(let i = n - 2; i >= 1; i -= 2) {
+        diamante += ' '.repeat((n - i) / 2) + '*'.repeat(i) + '\n';
+    }
+
+    console.log(diamante);
+    return diamante;
 
 }
 
+diamond(7);
