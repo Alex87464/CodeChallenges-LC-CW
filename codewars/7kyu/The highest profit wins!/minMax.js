ba35d504e5f61b04ccca425871378f691f23969a
Alex87464 <alexoliva87464@gmail.com>
2023-09-13 12:17:25 -0300

    Problem solved

--- codewars/7kyu/The highest profit wins!/minMax.js
+++ codewars/7kyu/The highest profit wins!/minMax.js
@@ -1,11 +1,8 @@
 // The highest profit wins!
 // https://www.codewars.com/kata/559590633066759614000063
 
-
-function minMax(arr){
-
-
-
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
 }
 
-minMax([1,2,3,4,5]); // [1,5]
+minMax([1, 2, 3, 4, 5]); // [1,5]
