fff2e27b85fd5367430215f2e6b7d604e1a41b98
Alex87464 <alexoliva87464@gmail.com>
2023-10-03 20:51:17 -0300

    New problem added

---
+++ codewars/7kyu/Shortest Word/findShort.js
@@ -0,0 +1,15 @@
+// Shortest Word
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+/*
+TODO:   Simple, given a string of words, return the length of the shortest word(s).
+?       String will never be empty and you do not need to account for different data types.
+*/
+
+
+function findShort(s){
+
+}
+
+
+findShort("bitcoin take over the world maybe who knows perhaps"); // Expected output -> 3
