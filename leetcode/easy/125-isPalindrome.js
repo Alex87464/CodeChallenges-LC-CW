104a3bb7855cafef8f9aa1b00af57b84398d1402
Alex87464 <alexoliva87464@gmail.com>
2023-10-05 10:15:05 -0300

    JS Problem solved

--- leetcode/easy/125-isPalindrome.js
+++ leetcode/easy/125-isPalindrome.js
@@ -3,10 +3,10 @@
 
 
 function isPalindrome(s) {
-    
+
     const cleanedStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     return cleanedStr === cleanedStr.split('').reverse().join('');
-    
+
 }
 
 isPalindrome("P0");
