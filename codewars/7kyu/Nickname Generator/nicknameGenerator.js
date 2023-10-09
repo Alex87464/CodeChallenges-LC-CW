c17dc10e1abeee5fbb1ec49215363de429b10b9d
Alex87464 <alexoliva87464@gmail.com>
2023-10-08 21:12:04 -0300

    JS Problem solved

---
+++ codewars/7kyu/Nickname Generator/nicknameGenerator.js
@@ -0,0 +1,41 @@
+// Nickname Generator
+// https://www.codewars.com/kata/593b1909e68ff627c9000186
+
+/* 
+Nickname Generator
+TODO Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+* If the 3rd letter is a consonant, return the first 3 letters.
+? nickname("Robert") //=> "Rob"
+? nickname("Kimberly") //=> "Kim"
+? nickname("Samantha") //=> "Sam"
+
+* If the 3rd letter is a vowel, return the first 4 letters.
+? nickname("Jeannie") //=> "Jean"
+? nickname("Douglas") //=> "Doug"
+? nickname("Gregory") //=> "Greg"
+
+* If the string is less than 4 characters 
+! return "Error: Name too short".
+
+ 
+? Notes:
+? Vowels are "aeiou", so discount the letter "y".
+? Input will always be a string.
+? Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+? The input can be modified
+*/
+
+function nicknameGenerator(name) {
+  if (name.length < 4) {
+    return "Error: Name too short";
+  }
+
+  if (vowels.includes(name[2])) {
+    return name.slice(0, 4);
+  }
+
+  return name.slice(0, 3);
+}
+
+nicknameGenerator("Douglas"); // Expected output -> Doug
