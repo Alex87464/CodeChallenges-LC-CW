765e25174213052b4ac152817b2479157e3982c0
Alex87464 <alexoliva87464@gmail.com>
2024-02-29 04:26:42 -0300

    Solved problem

--- codewars/7kyu/Mumbling/accum.js
+++ codewars/7kyu/Mumbling/accum.js
@@ -12,47 +12,12 @@ accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 */
 
-function accum(string) {
-
-    // Possible solution:
-    // 1. Convert the string to an array
-    const stringToArray = string.toLowerCase().split('');
-    let result = [];
-    // 2. Create an auxiliary variable to take the index+1 position of the array
-    let position = 0;
-    let counter = 0;
-    
-    // Traverse the array
-    for (let i = 0; i < stringToArray.length; i++) {
-
-        // Increase +1 the position variable
-        position += 1;
-
-        for(let k = 0; k < position; k++) {
-
-            // if( counter == position ) {
-            //     counter++;
-            //     // result.push('-')
-            //     result.push( stringToArray[i].toUpperCase() )
-            // } 
-
-            result.push(stringToArray[i]);
-        }
-    }
-    
-
-    // console.log(` the result string is: ${result}`)
-    console.log(` the result string is: ${result.join('')}`)
-
-    return result;
-
+function accum(str) {
+  return str
+    .split('')
+    .map((a, i) => a.toUpperCase() + a.toLowerCase().repeat(i))
+    .join('-');
 }
 
-accum("cwAt"); // Output -> "C-Ww-Aaa-Tttt"
-
-/* 
-Output examples:
-accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-accum("cwAt") -> "C-Ww-Aaa-Tttt" 
-*/
+console.log(accum('hola')); // Expected output -> "H-Oo-Lll-Aaaa"
+// accum('abcd'); // Expected output -> "A-Bb-Ccc-Dddd"
